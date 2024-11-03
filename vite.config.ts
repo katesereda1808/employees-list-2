34c8b0078ee5e3fedb8fd1c5be1aa9d4e6dedf1b
katesereda1808 <katesereda22@gmail.com>
2024-11-03 18:01:39 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/employees-list/',
+  base: '/employees-list-2/',
   server: {
     host: true,
     port: 5173,
